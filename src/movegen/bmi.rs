90ee6d0533592053cc41d8112c5fd9e7fe5841dd
Joe Roberts <josephroberts11@gmail.com>
2021-11-23 22:40:14 +0000

    Deal with compile warnings in bmi.rs

--- src/movegen/bmi.rs
+++ src/movegen/bmi.rs
@@ -1,10 +1,18 @@
-#[allow(unused)]
+// Allow over this entire module, as the conditional compilation causes a bunch of spurious unused
+// warnings without spamming the #[cfg(...)] attributes everywhere.
+#![allow(unused)]
+
+#[cfg(all(
+    any(target_arch = "x86", target_arch = "x86_64"),
+    target_feature = "bmi2"
+))]
+#[inline(always)]
 fn pext_native(val: u64, mask: u64) -> u64 {
-    pext_polyfill(val, mask)
+    todo!()
 }
 
 // Slower manual implementation of the PEXT behaviour for CPUs which don't support the instruction natively
-#[allow(unused)]
+#[inline(always)]
 fn pext_polyfill(val: u64, mut mask: u64) -> u64 {
     let mut res = 0;
     let mut bb = 1;
@@ -36,10 +44,13 @@ pub fn pext(val: u64, mask: u64) -> u64 {
 
 #[cfg(test)]
 mod test {
+    use super::*;
     use proptest::proptest;
 
-    use super::*;
-
+    #[cfg(all(
+        any(target_arch = "x86", target_arch = "x86_64"),
+        target_feature = "bmi2"
+    ))]
     proptest! {
         #[test]
         fn test_pext_polyfill(val: u64, mask: u64) {
